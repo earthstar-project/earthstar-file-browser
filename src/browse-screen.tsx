d4dd50d16ebbf6c2476cccc3d64fb57ab12fdb9e
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-10-11 08:01:08 -0700

    getting things into shape

---
+++ src/browse-screen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BrowseScreen() {
+    return <div className="App">This is the Browse screen</div>;
+}
+
+export default BrowseScreen;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import BrowseScreen from './browse-screen';
+
+import {
+    AuthorKeypair,
+    FormatValidatorEs4,
+    StorageAsync,
+    StorageDriverAsyncMemory
+} from 'stone-soup';
+
+//================================================================================
+// Earthstar setup
+
+let workspace = '+test.abc';
+let authorKeypair: AuthorKeypair = {
+    "address": "@demo.bki3qzbx5abimqw7on4y75ib7ybb4nks2vagmyvg2sfzj4dixmrca",
+    "secret": "bjcgip74umlnncva443572uyo7hsugm3a72ch2ej7d7o4epjgfa3a"
+}
+let storage = new StorageAsync(
+    workspace,
+    FormatValidatorEs4,
+    new StorageDriverAsyncMemory(workspace)
+);
+
+//================================================================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowseScreen />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
