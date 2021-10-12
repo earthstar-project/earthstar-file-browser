82acc828ab1c152f8a98b4d6631050cb75706985
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-10-11 21:11:16 -0700

    css mockup

--- src/browse-screen.tsx
+++ src/browse-screen.tsx
@@ -1,9 +1,116 @@
 import React from 'react';
 
-function BrowseScreen() {
-    return <div>
-        This is the Browse screen
-    </div>;
+let makeTempBorder = (enabled: boolean | undefined): string => {
+    return enabled
+        ? ` `//border-yellow-500 border-2 border-dashed `
+        : ' ';
 }
 
-export default BrowseScreen;
+// from https://mcdlr.com/8/
+//let triangleRightSm = '▸';
+//let triangleDownSm = '▾';
+//let triangleRightLg = '▶';
+let triangleDownLg = '▼';
+
+//================================================================================
+
+interface BasicProps {
+    className?: string;
+    tempBorder?: boolean;
+}
+
+export let BrowseScreen = () => {
+    return (
+        <div className='flex gap-8 flex-col bg-purple-400 fixed inset-0 p-3 overflow-y-hidden'>
+            <Earthbar className='flex-none' tempBorder={true}/>
+            <TreeAndDetail className='flex-auto' tempBorder={true}/>
+        </div>
+    );
+}
+
+export let Earthbar = (props: BasicProps) => {
+    let { className, tempBorder } = props;
+    return (
+        <div className={`${className ?? ''} ${makeTempBorder(tempBorder)} flex flex-wrap gap-1`}>
+            <button type="button" className='opacity-60 flex-none text-black px-2 py-1.5 rounded-xl font-bold border-none bg-white shadow-flat'>+workspace.2oifjqoj {triangleDownLg}</button>
+            <button type="button" className='opacity-60 flex-none text-black px-2 py-1.5 rounded-xl font-bold border-none bg-none'>Create...</button>
+            <button type="button" className='opacity-60 flex-none text-black px-2 py-1.5 rounded-xl font-bold border-none bg-none'>Join...</button>
+            <div className='flex-auto'/>
+            <button type="button" className='opacity-60 flex-none text-black px-2 py-1.5 rounded-xl font-bold border-none bg-none'>@cinn</button>
+        </div>
+    );
+}
+
+export let TreeAndDetail = (props: BasicProps) => {
+    let { className, tempBorder } = props;
+    let content = `
+I have eaten
+the plums
+that were in
+the icebox
+
+and which
+you were probably
+saving
+for breakfast
+
+Forgive me
+they were delicious
+so sweet
+and so cold
+
+  - William Carlos Williams`.trim();
+    return (
+        <div className={`${className ?? ''} ${makeTempBorder(tempBorder)} flex items-start gap-2`}>
+            <div className='flex-none w-40 bg-white max-h-screen p-2 rounded-xl overflow-y-auto'>
+                <div>/poems</div>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;160384.txt</div>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;160395.txt</div>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;160396.txt</div>
+                <div className='bg-green-200 -mx-2 px-2'>&nbsp;&nbsp;&nbsp;&nbsp;160398.txt</div>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;160399.txt</div>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;160403.txt</div>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;160409.txt</div>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;160412.txt</div>
+                <details className='px-3' open>
+                    <summary className='-mx-3'>/foo/</summary>
+                    <details className='px-3' open>
+                        <summary className='-mx-3'>bar/</summary>
+                        <details className='px-3' open>
+                            <summary className='-mx-3'>baz/</summary>
+                            <div>a.txt</div>
+                            <div>b.txt</div>
+                            <div>c.txt</div>
+                        </details>
+                        <div>x.jpg</div>
+                        <div>y.jpg</div>
+                    </details>
+                </details>
+            </div>
+            <div className='flex-auto bg-white max-h-screen rounded-xl overflow-y-auto'>
+                <div className='p-2 bg-green-200'>
+                    <h3>/poems/160398.txt</h3>
+                </div>
+                <div className='p-2'>
+                    <div className='h-3'/>
+                    <form className='flex flex-wrap gap-1'>
+                        <button type="button" className='shadow-flat flex-none text-purple-100 bg-purple-500 px-2 py-1.5 rounded-xl font-bold'> Delete </button>
+                        <button type="button" className='shadow-flat flex-none text-purple-100 bg-purple-500 px-2 py-1.5 rounded-xl font-bold'> Move </button>
+                        <button type="button" className='shadow-flat flex-none text-purple-100 bg-purple-500 px-2 py-1.5 rounded-xl font-bold'> Copy </button>
+                        <button type="button" className='shadow-flat flex-none text-purple-100 bg-purple-500 px-2 py-1.5 rounded-xl font-bold'> Download </button>
+                        <button type="button" className='shadow-flat flex-none text-purple-100 bg-purple-500 px-2 py-1.5 rounded-xl font-bold'> Blah </button>
+                        <button type="button" className='shadow-flat flex-none text-purple-100 bg-purple-500 px-2 py-1.5 rounded-xl font-bold'> Blah blah </button>
+                        <button type="button" className='shadow-flat flex-none text-purple-100 bg-purple-500 px-2 py-1.5 rounded-xl font-bold'> Blah blah </button>
+                        <button type="button" className='shadow-flat flex-none text-purple-100 bg-purple-500 px-2 py-1.5 rounded-xl font-bold'> Blah blah </button>
+                    </form>
+                    <div className='h-4'/>
+                    <div><span className='opacity-50'>Created by:</span> <span>@cinn</span></div>
+                    <div><span className='opacity-50'>Last modified</span> <span>Tuesday Oct 3, 9:31am</span></div>
+                    <div><span className='opacity-50'>Content:</span></div>
+                    <div className='h-3'/>
+                    <pre className='shadow-inner bg-green-200 p-4 -mx-2 whitespace-pre-wrap'>{content}</pre>
+                </div>
+            </div>
+        </div>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    boxShadow: {
+      flat:    '2px 6px 0px  -1px rgba(0, 0, 0, 0.15)',
+      sm:      '0 1px   2px     0 rgba(0, 0, 0, 0.25)',
+      DEFAULT: '0 3px   3px     0 rgba(0, 0, 0, 0.25), 0 1px 2px 0 rgba(0, 0, 0, 0.16)',
+      md:      '0 6px   4px  -1px rgba(0, 0, 0, 0.25), 0 2px 4px -1px rgba(0, 0, 0, 0.16)',
+      lg:      '0 10px 15px  -3px rgba(0, 0, 0, 0.25), 0 4px 6px -2px rgba(0, 0, 0, 0.15)',
+      xl:      '0 20px 25px  -5px rgba(0, 0, 0, 0.25), 0 10px 10px -5px rgba(0, 0, 0, 0.14)',
+      '2xl':   '0 25px 50px -12px rgba(0, 0, 0, 0.6),  0 10px 10px -5px rgba(0, 0, 0, 0.14)',
+      '3xl':   '0 35px 60px -15px rgba(0, 0, 0, 0.7),  0 10px 10px -5px rgba(0, 0, 0, 0.14)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      none: 'none',
+    },
     extend: {},
   },
   variants: {
